aba9b1bd1ec7e2ab74814aa4e05f17c200dcf56f
milxnaq <dupazlasu66@gmail.com>
2026-05-19 00:50:19 +0200

    build.sh: UNOFFICIAL -> AOSP
    Signed-off-by: milxnaq <dupazlasu66@gmail.com>

--- build.sh
+++ build.sh
@@ -350,9 +350,9 @@ BUILD_ZIP() {
     DATE="$(date +"%d-%m-%Y_%H-%M-%S")"
 
     if $KSU; then
-        NAME="${VERSION}_${MODEL}_KSU_$DATE.zip"
+        NAME="${VERSION}_${MODEL}_AOSP_KSU_$DATE.zip"
     else
-        NAME="${VERSION}_${MODEL}_$DATE.zip"
+        NAME="${VERSION}_${MODEL}_AOSP_$DATE.zip"
     fi
 
     if [[ -f "build/out/$MODEL/$NAME" ]]; then
